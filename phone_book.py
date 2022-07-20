2f44c1a730163b21d86f70d0d4203339bdbaadd0
Prince Johnson <prince.j@inapp.com>
2022-07-20 16:54:30 +0530

    minor changes in assignments

--- phone_book.py
+++ phone_book.py
@@ -22,14 +22,14 @@ while(True):
                 print(key)
         case 2:
             '''Add a new contact'''
-            name = input("Enter name :").title()
+            name = input("Enter name :")
             phone_number = input("Enter phone number :")
             phone_book[name] = phone_number
             print("Contact {} : {} added to phone book".format(name,phone_number))
         case 3:
             '''Delete a contact'''
-            name = input("enter name to delete :").title()
-            if(phone_book[name]==name):
+            name = input("enter name to delete :")
+            if(name in phone_book.keys()):
                 del phone_book[name]
                 print("contact {} deleted!...".format(name))
             else:
@@ -37,18 +37,19 @@ while(True):
         case 4:
             '''seach by contact by name'''
             name = input("Enter name to search: ")
-            if(phone_book[name] == name):
+            if name in phone_book.keys():
                 print("{}-{}".format(name,phone_book[name]))
             else:
                 print("no contacts found")
         case 5:
             '''search by number'''
             number = input("Enter phone number:")
-            numbers = phone_book.values()
+            numbers = list(phone_book.values())
+            #print(numbers)
             if(number in numbers):
                 pos = numbers.index(number)
-                names = phone_book.keys()
-                print(names[pos])
+                names = list(phone_book.keys())
+                print("Name :",names[pos])
             else:
                 print("number not found!")
         case 6:
